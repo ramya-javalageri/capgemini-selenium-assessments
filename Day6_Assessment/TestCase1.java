0fa725e1b096424b80d6cb4108b65a0a66f6ec5c
Ramya Javalageri <ramyajavalageri10@gmail.com>
2026-09-13 14:38:33 +0530

    Add TestCase1 for automated browser testing

---
+++ Day6_Assessment/TestCase1.java
@@ -0,0 +1,225 @@
+package Day6_Assessment;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class TestCase1 {
+
+	public static void main(String[] args) throws IOException, InterruptedException, AWTException {
+		
+		
+		//to fetch common  data from properties file
+		FileInputStream fis_p = new FileInputStream("./src/test/resources/Day6/TestCase1commondata.properties");
+
+		Properties p = new Properties();
+		
+		p.load(fis_p);
+		
+		String browser = p.getProperty("browser");
+		String url = p.getProperty("url");
+		String username = p.getProperty("username");
+		String password = p.getProperty("password");
+		
+		
+		//to fetch the data from excel file
+		FileInputStream fis_excel = new FileInputStream("./src/test/resources/Day6/TestCase1.xlsx");
+		
+		Workbook wb = WorkbookFactory.create(fis_excel);
+		String fn = wb.getSheet("Sheet1").getRow(1).getCell(0).getStringCellValue();
+		String ln = wb.getSheet("Sheet1").getRow(1).getCell(2).getStringCellValue();
+		String e_un = wb.getSheet("Sheet1").getRow(1).getCell(4).getStringCellValue();
+		String e_pwd =wb.getSheet("Sheet1").getRow(1).getCell(5).getStringCellValue();
+		String role = wb.getSheet("Sheet1").getRow(1).getCell(6).getStringCellValue();
+		String ename = wb.getSheet("Sheet1").getRow(1).getCell(7).getStringCellValue();
+		String status = wb.getSheet("Sheet1").getRow(1).getCell(8).getStringCellValue();
+		String a_us = wb.getSheet("Sheet1").getRow(1).getCell(9).getStringCellValue();
+		String a_pwd = wb.getSheet("Sheet1").getRow(1).getCell(10).getStringCellValue();
+		
+		
+		//launch the browser
+		WebDriver driver = null;
+		
+		if(browser.contains("chrome"))
+			driver = new ChromeDriver();
+		if(browser.contains("edge"))
+			driver = new EdgeDriver();
+		if(browser.contains("firefox"))
+			driver = new FirefoxDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+		driver.get(url);
+		
+		driver.findElement(By.name("username")).sendKeys(username);
+		
+		driver.findElement(By.name("password")).sendKeys(password);
+		
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+		
+		//select PIM
+		driver.findElement(By.xpath("//span[text()='PIM']")).click();
+		
+		//click on add 
+		driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary']")).click();
+		Thread.sleep(1000);
+		
+		//first name
+		driver.findElement(By.name("firstName")).sendKeys(fn);
+		Thread.sleep(1000);
+		
+		
+		//last name
+		driver.findElement(By.name("lastName")).sendKeys(ln);
+		Thread.sleep(1000);
+		
+		//login toggle button
+		driver.findElement(By.xpath("(//span[@class='oxd-switch-input oxd-switch-input--active --label-right'])")).click();
+		Thread.sleep(1000);
+		
+		//username
+		driver.findElement(By.xpath("(//input[@class='oxd-input oxd-input--active'])[3]")).sendKeys(e_un);
+		Thread.sleep(1000);
+		
+		//password
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys(e_pwd);
+		Thread.sleep(1000);
+		
+		//cofirm password
+		driver.findElement(By.xpath("(//input[@type='password'])[2]")).sendKeys(e_pwd);
+		Thread.sleep(1000);
+		
+		//save button
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Thread.sleep(2000);
+		
+		//click on admin
+		driver.findElement(By.xpath("//span[text()='Admin']")).click();
+		Thread.sleep(1000);
+		
+		//click on add
+		driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary']")).click();
+		Thread.sleep(1000);
+		
+		//select user role
+		WebElement role1 = driver.findElement(By.xpath("//div[@class='oxd-select-text-input']"));
+		role1.click();
+		Robot r = new Robot();
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
+		
+//		driver.findElement(By.xpath("//div[@role='option']//span[normalize-space()='"+role+"']")).click();
+		Thread.sleep(1000);
+		
+		//employee  name
+		WebElement employee = driver.findElement(By.xpath("//input[@placeholder='Type for hints...']"));
+		employee.sendKeys(ename);
+		Thread.sleep(1000);
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
+//		driver.findElement(By.xpath("//div[@role='listbox']//span[normalize-space()='" + ename + "']")).click();
+		Thread.sleep(1000);
+		
+		
+		//status
+		WebElement status1 = driver.findElement(By.xpath("(//div[@class='oxd-select-text-input'])[2]"));
+		status1.click();
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
+//		driver.findElement(By.xpath("//div[@role='option']//span[normalize-space()='" + status + "']")).click();
+		Thread.sleep(1000);
+		
+		//username
+		driver.findElement(By.xpath("(//input[@class='oxd-input oxd-input--active'])[2]")).sendKeys(a_us);
+		Thread.sleep(1000);
+		
+		//password
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys(a_pwd);
+		Thread.sleep(1000);
+		
+		//confirm password
+		driver.findElement(By.xpath("(//input[@type='password'])[2]")).sendKeys(a_pwd);
+		Thread.sleep(1000);
+		
+		//save button
+		driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space']")).click();
+		Thread.sleep(2000);
+		
+		//system user
+		driver.findElement(By.xpath("//span[text()='Admin']")).click();
+		Thread.sleep(1000);
+		
+		//username
+		driver.findElement(By.xpath("(//input[@class='oxd-input oxd-input--active'])[2]")).sendKeys(a_us);
+		Thread.sleep(1000);
+		
+		//user role
+		WebElement role2 = driver.findElement(By.xpath("//div[@class='oxd-select-text-input']"));
+		role2.click();
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
+//		driver.findElement(By.xpath("//div[@role='option']//span[normalize-space()='"+role+"']")).click();
+		Thread.sleep(1000);
+		
+		//employee name
+		WebElement employee1 = driver.findElement(By.xpath("//input[@placeholder='Type for hints...']"));
+		employee1.sendKeys(ename);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@role='listbox']//span[normalize-space()='" + ename + "']")).click();
+		Thread.sleep(1000);
+		
+		//status
+		WebElement status2 = driver.findElement(By.xpath("(//div[@class='oxd-select-text-input'])[2]"));
+		status2.click();
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
+//		driver.findElement(By.xpath("//div[@role='option']//span[normalize-space()='" + status + "']")).click();
+		Thread.sleep(1000);
+		
+		//search button
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Thread.sleep(1000);
+		
+		//verify
+		
+		if(!driver.findElement(By.xpath("//span[text()='No Records Found']")).isDisplayed())
+			System.out.println("Record found");
+		else
+			System.out.println("no record found");
+		
+		//close the browser
+		Thread.sleep(2000);
+		driver.quit();
+		
+		
+	}
+
+}
